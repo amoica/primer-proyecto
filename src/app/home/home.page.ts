567372f5d022b51db68325eb4bdca4857560cf83
amoica <innovaweb.ar@gmail.com>
2025-03-26 18:45:44 -0300

    ejemplo app-asist

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface Student {
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  present: boolean;
+  id?: number;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +16,52 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public actionSheetButtons = [
+    {
+      text: 'Ausente',
+      role: 'destructive',
+      data:{
+        action: 'delete'
+      },
+      icon: 'trash',
+    },
+    {
+      text: 'Presente',
+      role: 'present',
+      data:{
+        action: 'present'
+      },
+      icon: 'checkmark',
+    },
+    {
+      text:'Tarde',
+      role: 'late',
+      data:{
+        action: 'late'
+      },
+      icon: 'time',
+    },
+    {
+      text:'Cancelar',
+      role: 'cancel',
+      icon: 'close',
+      data: {
+        action: 'cancel'
+      }
+    }
+  ]
+
+  students: Student[] = [
+    { firstName: 'Juan', lastName: 'Pérez', avatar: 'https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-174669.jpg', present: false },
+    { firstName: 'María', lastName: 'García', avatar: 'https://img.freepik.com/free-vector/smiling-young-man-illustration_1308-173524.jpg', present: false },
+    { firstName: 'Carlos', lastName: 'Ramírez', avatar: 'https://img.freepik.com/free-vector/smiling-redhaired-cartoon-boy_1308-174709.jpg?semt=ais_hybrid', present: false },
+    { firstName: 'Ana', lastName: 'Sánchez', avatar: 'https://img.freepik.com/free-vector/blonde-boy-blue-hoodie_1308-175828.jpg', present: false },
+  ];
+
   constructor() {}
+
+  guardarAsistencia() {
+    console.log('Asistencia guardada:', this.students);
+  }
 
 }
